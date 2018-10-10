8d173396ab50faf14f1dcddad7a9ef50ca2e0211
Kritsada Mukdacharoenchai <kritsada.muk@gmail.com>
2018-10-10 16:42:37 +0700

    restore to original

--- bot.py
+++ bot.py
@@ -1,25 +1,27 @@
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
-import time
 import os
 
-Client = discord.Client()
+
 #GIVE YOUR BOT A PREFIX; mine is a.
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="a.")
+
+
+
 
 #PRINT THE DISCORD BOT'S NAME WHEN IT'S READY
 @bot.event
 async def on_ready():
-  print(bot.user.name)
+      print(bot.user.name)
 
-  
+        
 #A SIMPLE TEST COMMAND
 @bot.command(pass_context=True)
 async def hi(ctx):
-  await bot.say("Hello there"+" "+ctx.message.author.name)
-  
- 
+      await bot.say("Hello there"+" "+ctx.message.author.name)
+                
+                  
+                   
 #YOU CAN USE os.environ TO HIDE YOUR BOT TOKEN: SAVE YOUR BOT TOKEN AS THE NAME YOU GAVE IN os.environ['name'] 
 bot.run(os.environ['BOT_TOKEN'])
