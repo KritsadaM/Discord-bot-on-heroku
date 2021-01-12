d1d07cf7a94af9d30e64d87ae1c3916320d0e674
Kritsada Mukdacharoenchai <kritsada.mukdacharoenchai@lumentum.com>
2021-01-12 12:42:37 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,81 +58,81 @@ async def on_ready():
     print('Logged in as:\n{0} (ID: {0.id})'.format(bot.user))
     print('------')
 
-"""
-@bot.command(pass_context=True, no_pm=True)
-async def Wut():
-    """คำสั่งไว้หยอกเล่นกับวุดดี้ (ขำๆ นะเพื่อน)"""
-    await bot.say(random.choice(Wut_list) + " " + random.choice(possible_responses))
+# """
+# @bot.command(pass_context=True, no_pm=True)
+# async def Wut():
+#     """คำสั่งไว้หยอกเล่นกับวุดดี้ (ขำๆ นะเพื่อน)"""
+#     await bot.say(random.choice(Wut_list) + " " + random.choice(possible_responses))
 
-@bot.command(pass_context=True, no_pm=True)
-async def bitcoin():
-   """คำสั่งไว้เช็คราคา Bitcoin"""
-   url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
-   response = requests.get(url)
-   value = response.json() ['bpi']['USD']['rate']
-   await bot.say("Bitcoin price is: $" +value)
+# @bot.command(pass_context=True, no_pm=True)
+# async def bitcoin():
+#    """คำสั่งไว้เช็คราคา Bitcoin"""
+#    url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
+#    response = requests.get(url)
+#    value = response.json() ['bpi']['USD']['rate']
+#    await bot.say("Bitcoin price is: $" +value)
 
-class Ragnarok:
-    """Ragnarok Online"""
-    @commands.command(pass_context=True, no_pm=True)
-    async def rosearch(self, ctx, *, arg):
-        """คำสั่งเพื่อค้นหาข้อมูลจากเว็ป Database"""
-        search_message = ("https://revodb.prtwiki.in.th/search?q="+arg)
-        search_message = search_message.replace(" ", "%20")
-        await bot.say(search_message)
+# class Ragnarok:
+#     """Ragnarok Online"""
+#     @commands.command(pass_context=True, no_pm=True)
+#     async def rosearch(self, ctx, *, arg):
+#         """คำสั่งเพื่อค้นหาข้อมูลจากเว็ป Database"""
+#         search_message = ("https://revodb.prtwiki.in.th/search?q="+arg)
+#         search_message = search_message.replace(" ", "%20")
+#         await bot.say(search_message)
 
-    @commands.command(pass_context=True, no_pm=True)
-    async def นกดาเมจ(self, ctx):
-        """คำสั่งเปิดเว็ปคำนวนดาเมจของนกฮันเตอร์"""
-        await bot.say("https://misc.prtwiki.in.th/falcon")
+#     @commands.command(pass_context=True, no_pm=True)
+#     async def นกดาเมจ(self, ctx):
+#         """คำสั่งเปิดเว็ปคำนวนดาเมจของนกฮันเตอร์"""
+#         await bot.say("https://misc.prtwiki.in.th/falcon")
 
-    @commands.command(pass_context=True, no_pm=True)
-    async def trap(self, ctx):
-        """คำสั่งเปิดเว็ปคำนวนดาเมจของฮันเตอร์ trap"""
-        await bot.say("https://misc.prtwiki.in.th/trap")
+#     @commands.command(pass_context=True, no_pm=True)
+#     async def trap(self, ctx):
+#         """คำสั่งเปิดเว็ปคำนวนดาเมจของฮันเตอร์ trap"""
+#         await bot.say("https://misc.prtwiki.in.th/trap")
     
-    @commands.command(pass_context=True, no_pm=True)
-    async def อาหาร(self, ctx):
-        """คำสั่งเปิดเว็ปสำหรับทำอาหาร"""
-        await bot.say("https://www.prtwiki.in.th/wiki/cooking")
+#     @commands.command(pass_context=True, no_pm=True)
+#     async def อาหาร(self, ctx):
+#         """คำสั่งเปิดเว็ปสำหรับทำอาหาร"""
+#         await bot.say("https://www.prtwiki.in.th/wiki/cooking")
 
-    @commands.command(pass_context=True, no_pm=True)
-    async def เปลี่ยนอาชีพ(self, ctx):
-        """คำสั่งเปิดเว็ปเควสเปลี่ยนอาชีพ"""
-        await bot.say("https://www.prtwiki.in.th/wiki/job-change")
+#     @commands.command(pass_context=True, no_pm=True)
+#     async def เปลี่ยนอาชีพ(self, ctx):
+#         """คำสั่งเปิดเว็ปเควสเปลี่ยนอาชีพ"""
+#         await bot.say("https://www.prtwiki.in.th/wiki/job-change")
 
-    @commands.command(pass_context=True, no_pm=True)
-    async def แต่งงาน(self, ctx):
-        """คำสั่งเปิดเว็ปเควสแต่งงาน"""
-        await bot.say("https://www.prtwiki.in.th/wiki/wedding-system")
+#     @commands.command(pass_context=True, no_pm=True)
+#     async def แต่งงาน(self, ctx):
+#         """คำสั่งเปิดเว็ปเควสแต่งงาน"""
+#         await bot.say("https://www.prtwiki.in.th/wiki/wedding-system")
 
-    @commands.command(pass_context=True, no_pm=True)
-    async def เจาะรู(self, ctx):
-        """คำสั่งเปิดเว็ปวิธีเจาะรู"""
-        await bot.say("https://www.prtwiki.in.th/wiki/slot-enchant")
+#     @commands.command(pass_context=True, no_pm=True)
+#     async def เจาะรู(self, ctx):
+#         """คำสั่งเปิดเว็ปวิธีเจาะรู"""
+#         await bot.say("https://www.prtwiki.in.th/wiki/slot-enchant")
 
-    @commands.command(pass_context=True, no_pm=True)
-    async def ตารางธาตุ(self, ctx):
-        """คำสั่งเปิดเว็ปตารางแพ้ธาตุ"""
-        await bot.say("https://www.prtwiki.in.th/wiki/element")
+#     @commands.command(pass_context=True, no_pm=True)
+#     async def ตารางธาตุ(self, ctx):
+#         """คำสั่งเปิดเว็ปตารางแพ้ธาตุ"""
+#         await bot.say("https://www.prtwiki.in.th/wiki/element")
 
-    @commands.command(pass_context=True, no_pm=True)
-    async def เซตการ์ด(self, ctx):
-        """คำสั่งเปิดเว็ปเซตการ์ดของอาชีพต่างๆ"""
-        await bot.say("https://www.prtwiki.in.th/wiki/set-card")
+#     @commands.command(pass_context=True, no_pm=True)
+#     async def เซตการ์ด(self, ctx):
+#         """คำสั่งเปิดเว็ปเซตการ์ดของอาชีพต่างๆ"""
+#         await bot.say("https://www.prtwiki.in.th/wiki/set-card")
         
-    @commands.command(pass_context=True, no_pm=True)
-    async def สเตตัส(self, ctx):
-        """คำสั่งเปิดเว็ปคำนวนสเตตัส"""
-        await bot.say("https://stat.prtwiki.in.th/")
+#     @commands.command(pass_context=True, no_pm=True)
+#     async def สเตตัส(self, ctx):
+#         """คำสั่งเปิดเว็ปคำนวนสเตตัส"""
+#         await bot.say("https://stat.prtwiki.in.th/")
         
+#     @commands.command(pass_context=True, no_pm=True)
+#     async def exp(self, ctx):
+#         """คำสั่งเปิดเว็ปคำนวนสเตตัส"""
+#         await bot.say("https://www.prtwiki.in.th/wiki/base-exp-job-600921")
+# """
     @commands.command(pass_context=True, no_pm=True)
-    async def exp(self, ctx):
-        """คำสั่งเปิดเว็ปคำนวนสเตตัส"""
-        await bot.say("https://www.prtwiki.in.th/wiki/base-exp-job-600921")
-"""
-    @commands.command(pass_context=True, no_pm=True)
-    async def @(self, ctx, *, arg):
+    async def aaa(self, ctx, *, arg):
         """@@123 แปลงเลขฐานสำหรับ Central lab"""
         #search_message = ("https://revodb.prtwiki.in.th/search?q="+arg)
         #search_message = search_message.replace(" ", "%20")
@@ -142,10 +142,10 @@ class Ragnarok:
 
 bot.add_cog(Ragnarok())
 
-DecimalToBinary(num):
-        if num > 1:
-            DecimalToBinary(num // 2)
-            return num % 2
+def DecimalToBinary(num):
+    if num > 1:
+        DecimalToBinary(num // 2)
+        return num % 2
 
-#bot.run('NDk5NDU2ODEwNjIxNTk5NzQ0.Dp8r7A.2vbl1VRmCZUFJ8LOR_j2MgDXxhs')
+#bot.run('DV24555gNyZihn6XAKD_uBLkh9vlHCP6vJEE8LoGQOSKXj8BMVPoH_PSPwMIkfsiSrJf')
 bot.run(os.environ['BOT_TOKEN'])
