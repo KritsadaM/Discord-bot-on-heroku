bb7675f01b5ccce7997e6b5d4ed4680f9cf9234c
Kritsada Mukdacharoenchai <kritsada.mukdacharoenchai@lumentum.com>
2021-01-22 13:27:41 +0700

    Up

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
+import os
 import discord
-import requests
 import random
 from discord.ext import commands
-from discord.ext.commands import Bot
 import asyncio
 import os
 
@@ -22,14 +21,8 @@ async def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
-
-    if client.user.name in message.content:
-        if message.content == "bitcoin":
-            response = requests.get(url)
-            value = response.json() ['bpi']['USD']['rate']
-            await client.send(message.channel, "Bitcoin price is: $" + value)
-        if "@@" in message.content:
-            await client.send(message.channel, "Found it !!!")
+    if "@@" in message.content:
+        await client.send(message.channel, "Found it !!!")
     
     await client.send(message)
 
